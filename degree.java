2a722a446cde3b1b3af82e35806672360baa7d64
Deemssmeed <dxm0917@mavs.uta.edu>
2026-04-18 17:21:25 -0500

    Class Degree
    satisfiedCheck(); hasn't been complete bc it needs the other classes and objects. but comments are in there

---
+++ degree.java
@@ -0,0 +1,27 @@
+package stuff;
+
+class degree {  
+    private Category Category;
+    private String degreeName;
+    private boolean satisfied;
+
+    //Getters
+    public Category getCategory{return Category;}
+    public String getDegreeName{return degreeName;}
+    public boolean getSatisfied{return satisfied;}
+
+    //Setters
+    public void setCategory(Category){this.Category = category;}
+    public void setDegreeName(String){this.degree = degree;}
+    public void setSatisified(boolean){this.satisfied = satisfied;}
+
+    @Override
+    bool satisfiedCheck();
+    /*
+    how many groups are satified
+    (completed / amount of categories) * 100 = percentage
+
+    System.out.println("You have completed pecentage of" +  ___ + "on your degree so far.(Based out of how many
+    Requirement groups completed)");
+    */
+}
